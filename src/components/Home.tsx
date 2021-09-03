1c672a076e6bf1f0d2b2b17b19235a0bb6c168b5
Jarwin Marinas <jarwin.louranthy.marinas@prulifeuk.com.ph>
2021-09-03 08:46:06 +0800

    initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React, { useReducer, useEffect } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+
+
+const Home = () => {
+    return ( <TextField/>
+      );
+
+}
+
+export default Home;
